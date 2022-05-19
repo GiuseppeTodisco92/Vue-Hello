94eec8d44c4daf637f3091c3344d6a4b65c42b16
GiuseppeTodisco92 <giuseppe24todisco92@gmail.com>
2022-05-19 14:17:47 +0100

    add message

---
+++ js/main.js
@@ -0,0 +1,25 @@
+const app = new Vue({
+
+    el: '#app',
+    data :{
+        message : 'Hello World',
+    },
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Descrizione:
+// Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+// Bonus:
+// Aggiungere alla pagina un’immagine, presa anch’essa da un data.
